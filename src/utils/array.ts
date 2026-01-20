ad93606999804b553a2a23d23298ab5a224616b4
Mayron <mayron.fernandes@alghorit.com.br>
2026-01-20 13:04:46 -0300

    docs: add docs to src/utils

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,3 +1,23 @@
+/**
+ * @file Contains utility functions for working with arrays.
+ */
+
+/**
+ * Merges multiple arrays of objects into a single array, ensuring uniqueness based on a specified key.
+ * If multiple objects have the same value for the given key, the last one encountered in the input arrays prevails.
+ *
+ * @template T The type of objects in the arrays.
+ * @template K The key of the object to merge by.
+ * @param {K} key The key to use for merging.
+ * @param {...T[][]} lists The arrays of objects to merge.
+ * @returns {T[]} A new array containing the merged, unique objects.
+ *
+ * @example
+ * const arr1 = [{ id: 1, name: 'A' }, { id: 2, name: 'B' }];
+ * const arr2 = [{ id: 2, name: 'C' }, { id: 3, name: 'D' }];
+ * const merged = mergeBy('id', arr1, arr2);
+ * // merged will be [{ id: 1, name: 'A' }, { id: 2, name: 'C' }, { id: 3, name: 'D' }]
+ */
 export const mergeBy = <T, K extends keyof T>(key: K, ...lists: T[][]): T[] => {
   const map = new Map<T[K], T>();
 
--- src/utils/get-all-class-methods.ts
+++ src/utils/get-all-class-methods.ts
@@ -1,3 +1,26 @@
+/**
+ * @file Provides a utility function for introspecting class methods.
+ */
+
+/**
+ * Retrieves all method names (excluding the constructor) from a given class instance or its prototype chain.
+ * This function iterates up the prototype chain to collect methods defined at various levels.
+ *
+ * @param {any} target The class constructor or an instance of the class.
+ * @returns {string[]} An array of strings, where each string is the name of a method.
+ *
+ * @example
+ * class MyClass {
+ *   methodA() {}
+ *   methodB() {}
+ * }
+ * class AnotherClass extends MyClass {
+ *   methodC() {}
+ * }
+ *
+ * getAllClassMethods(new AnotherClass()); // ['methodC', 'methodA', 'methodB']
+ * getAllClassMethods(AnotherClass);      // ['methodC', 'methodA', 'methodB']
+ */
 export const getAllClassMethods = (target: any): string[] => {
   const methods: string[] = [];
   let proto = target.prototype;
